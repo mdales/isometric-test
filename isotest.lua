4f56c65c150f0a28e7f090afc73bc961cdc05a5e
Michael Dales <michael@digitalflapjack.com>
2022-12-29 15:47:47 +0000

    Use perlin noise to generate map.

--- isotest.lua
+++ isotest.lua
@@ -5,6 +5,124 @@ t=0
 W=240
 H=136
 
+-- begin perlin lib code
+perlin = {}
+perlin.ken = {151,160,137,91,90,15,
+  131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
+  190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
+  88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
+  77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
+  102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
+  135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
+  5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
+  223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
+  129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
+  251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
+  49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+  138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
+}
+
+function perlin:permutation(x)
+  -- Hash lookup table as defined by Ken Perlin
+  -- This is a randomly arranged array of all numbers from 0-255 inclusive
+  return perlin.ken[(x%0xFF)+1]
+end
+
+-- Return range: [-1, 1]
+function perlin:noise(x, y, z)
+    y = y or 0
+    z = z or 0
+
+    -- Calculate the "unit cube" that the point asked will be located in
+    local xi = math.floor(x)&255
+    local yi = math.floor(y)&255
+    local zi = math.floor(z)&255
+
+    -- Next we calculate the location (from 0 to 1) in that cube
+    x = x - math.floor(x)
+    y = y - math.floor(y)
+    z = z - math.floor(z)
+
+    -- We also fade the location to smooth the result
+    local u = self.fade(x)
+    local v = self.fade(y)
+    local w = self.fade(z)
+
+    -- Hash all 8 unit cube coordinates surrounding input coordinate
+    local A, AA, AB, AAA, ABA, AAB, ABB, B, BA, BB, BAA, BBA, BAB, BBB
+    A   = perlin:permutation(xi) + yi
+    AA  = perlin:permutation(A) + zi
+    AB  = perlin:permutation(A+1) + zi
+    AAA = perlin:permutation(AA)
+    ABA = perlin:permutation(AB)
+    AAB = perlin:permutation(AA+1)
+    ABB = perlin:permutation(AB+1)
+
+    B   = perlin:permutation(xi+1) + yi
+    BA  = perlin:permutation(B) + zi
+    BB  = perlin:permutation(B+1) + zi
+    BAA = perlin:permutation(BA)
+    BBA = perlin:permutation(BB)
+    BAB = perlin:permutation(BA+1)
+    BBB = perlin:permutation(BB+1)
+
+    -- Take the weighted average between all 8 unit cube coordinates
+    return self.lerp(w,
+        self.lerp(v,
+            self.lerp(u,
+                self:grad(AAA,x,y,z),
+                self:grad(BAA,x-1,y,z)
+            ),
+            self.lerp(u,
+                self:grad(ABA,x,y-1,z),
+                self:grad(BBA,x-1,y-1,z)
+            )
+        ),
+        self.lerp(v,
+            self.lerp(u,
+                self:grad(AAB,x,y,z-1), self:grad(BAB,x-1,y,z-1)
+            ),
+            self.lerp(u,
+                self:grad(ABB,x,y-1,z-1), self:grad(BBB,x-1,y-1,z-1)
+            )
+        )
+    )
+end
+
+-- Gradient function finds dot product between pseudorandom gradient vector
+-- and the vector from input coordinate to a unit cube vertex
+perlin.dot_product = {
+    [0x0]=function(x,y,z) return  x + y end,
+    [0x1]=function(x,y,z) return -x + y end,
+    [0x2]=function(x,y,z) return  x - y end,
+    [0x3]=function(x,y,z) return -x - y end,
+    [0x4]=function(x,y,z) return  x + z end,
+    [0x5]=function(x,y,z) return -x + z end,
+    [0x6]=function(x,y,z) return  x - z end,
+    [0x7]=function(x,y,z) return -x - z end,
+    [0x8]=function(x,y,z) return  y + z end,
+    [0x9]=function(x,y,z) return -y + z end,
+    [0xA]=function(x,y,z) return  y - z end,
+    [0xB]=function(x,y,z) return -y - z end,
+    [0xC]=function(x,y,z) return  y + x end,
+    [0xD]=function(x,y,z) return -y + z end,
+    [0xE]=function(x,y,z) return  y - x end,
+    [0xF]=function(x,y,z) return -y - z end
+}
+function perlin:grad(hash, x, y, z)
+    return self.dot_product[hash&0xF](x,y,z)
+end
+
+-- Fade function is used to smooth final output
+function perlin.fade(t)
+    return t * t * t * (t * (t * 6 - 15) + 10)
+end
+
+function perlin.lerp(t, a, b)
+    return a + t * (b - a)
+end
+-- end perlin lib code
+
 chunks={}
 entities={}
 
@@ -16,38 +134,24 @@ mapH=16*8
 cx=mapW//2
 cy=mapH//2
 cd=0 --n,e,s,w
-vd=0 
+vd=0
 
 function worldgen(chunk)
-  xc=((chunk>>16)-0x7fff)*16
-  yc=((chunk&0xFFFF)-0x7fff)*16
+  xc=((chunk>>16)-0x7fff)
+  yc=((chunk&0xFFFF)-0x7fff)
   seed=pmem(0)
-  math.randomseed(seed+xc*yc)
   m={}
   for y=0,15 do
     r={}
     for x=0,15 do
-      ix=xc+x
-      iy=yc+y
       c=(math.random()*2)+5
-      h=math.abs(
-        (
-          math.cos(
-            (ix+iy/2)/(math.pi/1)
-          )*2
-          +
-          math.sin(
-            (ix/3+iy/3)/(math.pi/2)
-          )*2
-        )
-        //1
-      )
+      h=3+((perlin:noise(xc+(x/16),yc+(y/16),seed)*8)//1)
       if h>0 then
-		      if math.random()<0.1 then
-		        c=7
-		      end
-		    end
-						r[x]={colour=c,height=h}
+        if math.random()<0.1 then
+          c=7
+        end
+      end
+      r[x]={colour=c,height=h}
     end
     m[y]=r
   end
@@ -81,6 +185,10 @@ end
 function tile(x, y, cell)
   c=cell.colour
   h=cell.height
+  if h<=0 then
+    c=10+h
+    h=0
+  end
   player=x==5 and y==5
   hs=10
   vs=5
@@ -88,9 +196,6 @@ function tile(x, y, cell)
   xo=(W/2)+(x*hs)-(y*hs)
   yb=H-(10+(x*vs)+(y*vs))
   yo=yb-(h*ds)
-  if h==0 then
-    c=5+c
-  end
   tree=c==7
   if tree then c=5 end
   tri(
@@ -192,7 +297,7 @@ function drawmap()
         ay=yc+y
         col=r[x].colour
         h=r[x].height
-        if h<1 then col=5+col end
+        if h<1 then col=10+h end
         if ax==cx and ay==cy then col=2 end
         pix(
           ax+(W-mapW)//2,
